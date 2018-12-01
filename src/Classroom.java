452f5b7d879d03dfe6fcb2187de6eaa816f8057c
Nick-Trag <ntragkas@csd.auth.gr>
2018-12-01 10:43:04 +0200

    Added course and classroom classes

---
+++ src/Classroom.java
@@ -0,0 +1,36 @@
+public class Classroom
+{
+    /*private Long id;
+    private String name;
+    private Long depId;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public Long getDepId()
+    {
+        return depId;
+    }
+
+    public void setDepId(Long depId)
+    {
+        this.depId = depId;
+    }*/
+}
